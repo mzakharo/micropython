a686cc98bb2b74b551055b07994fb0448006c2cd
Mikhail Zakharov <mzakharo@gmail.com>
2023-02-09 04:27:01 -0500

    add REAMDE, cleanup

---
+++ ports/esp32/README.me.md
@@ -0,0 +1,21 @@
+Prepare build:
+
+git clone -b v4.4.4 --recursive https://github.com/espressif/esp-idf.git
+source esp-idf/export.sh
+git submodule update --init --recursive
+
+Build:
+make
+
+Erase flash:
+make erase
+
+Deploy:
+make deploy
+
+Publish firmware for OTA:
+python3 publish_fw.py  build-UM_FEATHERS2/micropython.bin 
+
+To access webrepl:
+https://github.com/mzakharo/upydev
+
--- ports/esp32/modules/main.py
+++ ports/esp32/modules/main.py
@@ -46,6 +46,7 @@ DISABLE_LIGHTSLEEP = True
 DISABLE_DEEPSLEEP = False
 DISABLE_DEEPSLEEP = True
 
+#sleep due to wlan connect error
 SLEEP_FAST = 60_000
 
 WIFI_SSID = 'wireless_ext'
@@ -53,14 +54,15 @@ if PROFILING:
     WIFI_SSID = 'wireless'
 WIFI_PASSWORD = 'W@terl004ever'
 MQTT_SERVER = '192.168.50.96'
-MQTT_TOPIC = 'feather/status'
-if PROFILING:
-    MQTT_TOPIC = 'feather/profiling'
 
+MQTT_OTA_CMD_TOPIC = 'tubby/ota/cmd'
+MQTT_OTA_FW_TOPIC = 'tubby/ota/fw'
+MQTT_TOPIC = 'tubby/status'
+if PROFILING:
+    MQTT_TOPIC = 'tubby/profiling'
 
 ORP_PIN = const(1)
 BAT_PIN = const(3)
-LDO2 = const(21)
 
 #import esp
 #esp.osdebug(0, esp.LOG_DEBUG)
@@ -94,15 +96,70 @@ def my_sleep_ms(duration):
         lightsleep(duration)
 
 set_led(False) #disable for low power
-o = ADC(Pin(ORP_PIN)) #Yellow 
+
+o = ADC(Pin(ORP_PIN)) 
 o.atten(ADC.ATTN_11DB)    # set 11dB input attenuation (voltage range roughly 0.0v - 3.6v)
-o.width(ADC.WIDTH_13BIT)
+#o.width(ADC.WIDTH_13BIT)
 
-b = ADC(Pin(BAT_PIN)) #Yellow 
+b = ADC(Pin(BAT_PIN)) 
 b.atten(ADC.ATTN_11DB)    # set 11dB input attenuation (voltage range roughly 0.0v - 3.6v)
-b.width(ADC.WIDTH_13BIT)
+#b.width(ADC.WIDTH_13BIT)
 
-uid = random_string()
+
+class State:
+    def __init__(self):
+        self.prevent_sleep = False
+        self.subscribe_ota = False
+        self.measured = False
+        self.check_sha = b''
+        self.check = {}
+        try:
+            with open('check_sha.txt', 'r') as f:
+                self.check = json.loads(f.read())
+        except Exception as e:
+            print('check err', e)
+
+s = State()
+
+def on_message(topic, msg):
+    topic = topic.decode()
+    print('received', topic, msg[:100])
+    if topic == MQTT_OTA_CMD_TOPIC:
+        if msg == b'webrepl':
+            print('enabling webrepl')
+            import webrepl
+            webrepl.start(password='admin')
+            s.prevent_sleep = True
+        else:
+            sha = msg.decode()
+            part = Partition(Partition.RUNNING).info()[4]
+            my_sha = s.check.get('sha', '')
+            my_part = s.check.get('part', '')
+
+            if my_sha == sha and my_part == part:
+                print('OTA skip: already running this image')
+                return
+
+            print(f'OTA Queue: my_sha: {my_sha}, sha: {sha} my_part: {my_part} part: {part}')
+            s.prevent_sleep = True
+            s.subscribe_ota = True
+            s.check_sha = sha
+
+    elif topic == MQTT_OTA_FW_TOPIC:
+        print('OTA write: fw len:', len(msg))
+        s.prevent_sleep = False
+        from ota import OTA
+        fw = io.BytesIO(msg)
+        ot = OTA(fw, 0, check_sha=s.check_sha, tls=True)
+        BLOCKLEN = 4096
+        n_blocks = (len(msg) // BLOCKLEN) + 1
+        ot.do_ota(blocks=n_blocks, debug=True)
+        ot.check_ota()
+        with open('check_sha.txt', 'w') as f:
+            f.write(json.dumps({'sha': s.check_sha, 'part' :  Partition(Partition.BOOT).info()[4]}))
+        print('Rebooting device...')
+        time.sleep(1)
+        reset()
 
 wlan = network.WLAN(network.STA_IF)
 
@@ -124,98 +181,15 @@ def wifi():
     print('connected in ', diff)
     return wlan
 
-'''
-while True:
-    wlan = wifi()
-    wlan.disconnect()
-    wlan.active(False)
-    wdt.feed()
-'''
-
-client = MQTTClient(client_id=uid, server=MQTT_SERVER)
-
-class State:
-    def __init__(self):
-        self.prevent_sleep = False
-        self.subscribe_ota = False
-        self.measured = False
-        self.check_sha = b''
-        self.check = {}
-        try:
-            with open('check_sha.txt', 'r') as f:
-                self.check = json.loads(f.read())
-        except Exception as e:
-            print('check err', e)
-
-
-
-s = State()
-
-def on_message(topic, msg):
-    print('received', topic, msg[:100])
-    if topic == b'tubby/ota/cmd':
-        if msg == b'webrepl':
-            print('enabling webrepl')
-            import webrepl
-            webrepl.start(password='admin')
-            s.prevent_sleep = True
-        else:
-            sha = msg.decode()
-            part = Partition(Partition.RUNNING).info()[4]
-            my_sha = s.check.get('sha', '')
-            my_part = s.check.get('part', '')
-
-            if my_sha == sha and my_part == part:
-                print('OTA skip: already running this image')
-                return
-
-            print(f'OTA Queue: my_sha: {my_sha}, sha: {sha} my_part: {my_part} part: {part}')
-            s.prevent_sleep = True
-            s.subscribe_ota = True
-            s.check_sha = sha
-
-    elif topic == b'tubby/ota/fw':
-        print('OTA write: fw len:', len(msg))
-        s.prevent_sleep = False
-        from ota import OTA
-        fw = io.BytesIO(msg)
-        ot = OTA(fw, 0, check_sha=s.check_sha, tls=True)
-        BLOCKLEN = 4096
-        n_blocks = (len(msg) // BLOCKLEN) + 1
-        ot.do_ota(blocks=n_blocks, debug=True)
-        ot.check_ota()
-        with open('check_sha.txt', 'w') as f:
-            f.write(json.dumps({'sha': s.check_sha, 'part' :  Partition(Partition.BOOT).info()[4]}))
-        print('Rebooting device...')
-        time.sleep(1)
-        reset()
-
+client = MQTTClient(client_id=random_string(), server=MQTT_SERVER)
 
 def mqtt():
     wifi()
     #print('network config:', wlan.ifconfig())
     client.set_callback(on_message)
     client.connect()
-    client.subscribe('tubby/ota/cmd')
+    client.subscribe(MQTT_OTA_CMD_TOPIC)
     return client
-
-def measure5():
-    status = {}
-    set_ldo2_power(True)
-    wdt.feed()
-    my_sleep_ms(ORP_SLEEP)
-    wdt.feed()
-    orp = o.read() - 1500
-    set_ldo2_power(False)
-    status[f'orp60'] = orp
-    #get battery voltage
-    vbat = b.read() * 2
-    status['vbat'] = float(vbat)
-    status['sha'] = s.check.get('sha', '')
-    status['ver'] = uversion
-    publish(status)
-    return True
-
 
 def publish(status):
     mqtt()
@@ -225,16 +199,33 @@ def publish(status):
         client.publish(MQTT_TOPIC, json.dumps(status), qos=1)
 
 
+def measure():
+    status = {}
+    set_ldo2_power(True)
+    wdt.feed()
+    my_sleep_ms(ORP_SLEEP)
+    wdt.feed()
+    orp = o.read_uv() // 1000 - 1500
+    set_ldo2_power(False)
+    status[f'orp'] = orp
+    vbat = b.read_uv() // 1000 * 2
+    status['vbat'] = vbat
+    status['sha'] = s.check.get('sha', '')
+    status['ver'] = uversion
+    publish(status)
+    return True
+
+
 while True:
     wdt.feed()
 
     if not s.measured:
-        s.measured = measure5()
+        s.measured = measure()
         Partition.mark_app_valid_cancel_rollback()
 
     if s.subscribe_ota:
         s.subscribe_ota = False
-        client.subscribe('tubby/ota/fw')
+        client.subscribe(MQTT_OTA_FW_TOPIC)
 
     if s.prevent_sleep:
         client.check_msg()
