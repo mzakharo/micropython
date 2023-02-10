9b4186c7476c2e54705add7146d6dc0bb1c08bf4
Mikhail Zakharov <mzakharo@gmail.com>
2023-02-10 08:03:39 -0500

    add timestamp to logs

--- ports/esp32/logs.py
+++ ports/esp32/logs.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 import modules.config as config 
+import datetime
 
 client = mqtt.Client()
 client.connect(config.MQTT_SERVER);
@@ -8,7 +9,7 @@ def on_connect(client, a, b, c):
     mytopic = f'{config.NAME}/logs'
     client.subscribe(mytopic)
 def on_message(client, userdata, message,tmp=None):
-    print(message.payload.decode())
+    print(datetime.datetime.now(), message.payload.decode())
 client.on_message = on_message;
 client.on_connect = on_connect
 
