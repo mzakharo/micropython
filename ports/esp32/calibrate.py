6acd0a7a47da7d712d25ffcea50acfc1cab80bc0
Mikhail Zakharov <mzakharo@gmail.com>
2024-01-02 08:07:43 -0500

    fix

--- ports/esp32/calibrate.py
+++ ports/esp32/calibrate.py
@@ -3,9 +3,9 @@ import sys
 import modules.config as config 
 import modules.secrets as secrets
 
-SLEEP = int(sys.argv[1])
 
-if len(sys.argv) == 2:
+if len(sys.argv) == 2:    
+    SLEEP = int(sys.argv[1])
     print('publishing sleep', SLEEP)
     publish.single(f'{config.NAME}/sleep', str(SLEEP), qos=1, retain=True, hostname=secrets.MQTT_SERVER)
 else:
