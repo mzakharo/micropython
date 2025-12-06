5c8138a40470343ef710bdfa046868f5d926e817
Mikhail Zakharov <mzakharo@gmail.com>
2025-12-05 23:02:56 -0500

    old modules

---
+++ ports/esp32/modules/app.py
@@ -0,0 +1,408 @@
+import utime as time
+import ujson as json
+import uos
+import io
+from machine import deepsleep, lightsleep, unique_id
+from feathers2 import set_ldo2_power, set_led, AMB_LIGHT
+from machine import ADC, Pin, reset
+from esp32 import Partition
+import config
+import secrets
+import network
+import tfmicro #custom user module for tflite-micro models
+
+#Calibration constants 
+
+#ORP Probe
+ORP_CAL_OFFSET = 21
+
+#PH Probe
+PH_MID_CAL = 1533
+PH_LOW_CAL = 2031
+PH_HIGH_CAL = 1042
+
+#Battery sensing
+BAT_LOW = 3000
+BAT_HIGH = 4200
+
+# free chlorine to Total Bromine
+FC_TO_TOTAL_BROMINE = 2.25
+
+
+#DEBUG config constants
+
+PROFILING=False
+#PROFILING=True
+
+CALIBRATION=False
+#CALIBRATION=True
+
+if CALIBRATION:
+    PROFILING = False
+
+DISABLE_LIGHTSLEEP = False
+#DISABLE_LIGHTSLEEP = True
+
+#disable deep sleep
+DISABLE_DEEPSLEEP = False
+#DISABLE_DEEPSLEEP = True
+
+
+
+
+NUM_SAMPLES = 1000 #from Atlas Scientific sample code
+
+#how long to sleep after measurement
+SLEEP = 900_000
+if PROFILING or CALIBRATION:
+    SLEEP = 5_000
+
+#how long to wait for sensor to settle after power on from deep sleep
+#NOTE: for calibration of ORP 200 solution, minimum time to settle is 60s
+
+SENSOR_SETTLE_SLEEP = 100_000
+SENSOR_SETTLE_SLEEP_TIMES = 9
+if PROFILING:
+    SENSOR_SETTLE_SLEEP = 3_000
+    SENSOR_SETTLE_SLEEP_TIMES = 1
+
+#sleep due to wlan connect error
+SLEEP_FAST = 60_000
+
+NODE_ID = "".join("%x" % b for b in unique_id())
+
+name = config.NAME
+MQTT_OTA_CMD_TOPIC = f'{name}/{NODE_ID}/ota/cmd'
+MQTT_OTA_FW_TOPIC = f'{name}/{NODE_ID}/ota/fw'
+MQTT_STATE_TOPIC = f'{name}/{NODE_ID}/status'
+
+#pybalboa temperature status published by https://github.com/mzakharo/pybalboa/blob/master/main.py
+MQTT_BALBOA_TOPIC = f'balboa/temp'
+
+MQTT_SLEEP_TOPIC = f'{name}/sleep'
+
+
+TEMP_CELSIUS = "°C"
+
+ORP_PIN = const(1)
+PH_PIN = const(3)
+BAT_PIN = const(7) # Voltage divider between BAT_PIN and VBAT_PIN & Ground. Resistors: Brown Black Yellow.
+
+def wifi(wdt, ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    s = time.ticks_ms()
+
+    wlan.active(True)
+
+    if not wlan.isconnected():
+        wlan.connect(ssid, password)
+
+    diff = 0
+    while not wlan.isconnected():
+        diff = time.ticks_diff(time.ticks_ms(), s)
+        if diff > 15_000:
+            my_go_deepsleep(SLEEP_FAST)
+        else:
+            wdt.feed()
+    print('connected in ', diff)
+
+def mean(v):
+    return sum(v) / len(v)
+
+def median(v):
+    l = sorted(v)
+    return l[len(v)//2+1]
+
+def my_go_deepsleep(duration):
+    #Pin(ORP_PIN, Pin.IN, Pin.PULL_DOWN, hold=True)
+    #Pin(PH_PIN, Pin.IN, Pin.PULL_DOWN, hold=True)
+    #Pin(BAT_PIN, Pin.IN, Pin.PULL_UP, hold=True)
+    #Pin(AMB_LIGHT, Pin.IN, Pin.PULL_UP, hold=True)
+    #deepsleep(duration)
+    from feathers2 import go_deepsleep
+    go_deepsleep(duration)
+
+
+def my_sleep_ms(duration):
+    if DISABLE_LIGHTSLEEP:
+        time.sleep_ms(duration)
+    else:
+        lightsleep(duration)
+
+class State:
+    def __init__(self):
+        self.prevent_sleep = False
+        self.subscribe_ota = False
+        self.measured = False
+        self.check_sha = b''
+        self.check = {}
+        self.temp = 40.0
+        self.sleep = SLEEP
+        self.settle = SENSOR_SETTLE_SLEEP
+        try:
+            with open('check_sha.txt', 'r') as f:
+                self.check = json.loads(f.read())
+        except Exception as e:
+            print('check err', e)
+
+#automatic temperature compensation
+# source: https://docs.rbr-global.com/support/instruments/compensation-equations/ph-simple-temperature-correction-of-ph
+def atc(ph, temp):
+    return ph - 0.0032 * (ph - 7.0) * (temp - 25.0)
+
+
+
+def run(client, wdt):
+
+    uversion = uos.uname().version
+    print('FW version: ', uversion)
+    print('Node ID: ', NODE_ID)
+
+    #import esp
+    #esp.osdebug(0, esp.LOG_DEBUG)
+    #tic = time.ticks_ms()
+
+    #ADC.ATTN_0DB: No attenuation (100mV - 950mV)
+    #ADC.ATTN_2_5DB: 2.5dB attenuation (100mV - 1250mV)
+    #ADC.ATTN_6DB: 6dB attenuation (150mV - 1750mV)
+    #ADC.ATTN_11DB: 11dB attenuation (150mV - 2450mV)
+
+    ao = ADC(Pin(ORP_PIN), atten=ADC.ATTN_11DB) 
+    ap = ADC(Pin(PH_PIN),  atten=ADC.ATTN_11DB) 
+    ab = ADC(Pin(BAT_PIN), atten=ADC.ATTN_11DB) 
+
+    set_led(False) #disable for low power
+
+    s = State()
+
+    def on_message(topic, msg):
+        topic = topic.decode()
+        #print('received', topic, msg[:100])
+        if topic == MQTT_OTA_CMD_TOPIC:
+            if msg == b'webrepl':
+                print('enabling webrepl')
+                import webrepl
+                webrepl.start(password='admin')
+                s.prevent_sleep = True
+            else:
+                sha = msg.decode()
+                part = Partition(Partition.RUNNING).info()[4]
+                my_sha = s.check.get('sha', '')
+                my_part = s.check.get('part', '')
+
+                if my_sha == sha and my_part == part:
+                    print('OTA skip: already running this image:', sha[:8])
+                    return
+
+                print(f'OTA Queue: my_sha: {my_sha}, sha: {sha} my_part: {my_part} part: {part}')
+                s.prevent_sleep = True
+                s.subscribe_ota = True
+                s.check_sha = sha
+
+        elif topic == MQTT_OTA_FW_TOPIC:
+            print('OTA write: fw len:', len(msg))
+            s.prevent_sleep = False
+            wdt.feed()
+            from ota import OTA
+            fw = io.BytesIO(msg)
+            ot = OTA(fw, 0, check_sha=s.check_sha, tls=True)
+            BLOCKLEN = 4096
+            n_blocks = (len(msg) // BLOCKLEN) + 1
+            ot.do_ota(blocks=n_blocks, debug=True)
+            ot.check_ota()
+            with open('check_sha.txt', 'w') as f:
+                f.write(json.dumps({'sha': s.check_sha, 'part' :  Partition(Partition.BOOT).info()[4]}))
+            print('Rebooting device...')
+            time.sleep(1)
+            reset()
+        elif topic == MQTT_BALBOA_TOPIC:
+            s.temp = float(msg)
+            #print('Balboa temp %f' % s.temp)
+        elif topic == MQTT_SLEEP_TOPIC:
+            try:
+                s.sleep = int(msg)
+                s.settle = s.sleep
+                print('New sleep duration %d' % s.sleep)
+            except Exception as e:
+                print(e)
+
+
+    def discovery(client):
+        model = uos.uname().machine
+        sha = s.check.get('sha', '')[:8]
+
+        expire = 3 * (SLEEP + SENSOR_SETTLE_SLEEP * SENSOR_SETTLE_SLEEP_TIMES)  // 1000
+        device =  {
+                            'identifiers' : NODE_ID,
+                            'name' : name,
+                            'sw_version' : uversion + '_' + sha,
+                            'model' : model,
+                            'manufacturer' : NODE_ID, # for HA UI  -> easy display
+                        }
+
+        t = f'homeassistant/sensor/{NODE_ID}/{name}_battery/config'
+        m = {'device_class' : 'battery',
+            'unit_of_measurement': '%',
+            'icon' : 'mdi:battery',
+            'name': f'Battery',
+            'state_topic': MQTT_STATE_TOPIC,
+            'unique_id' : f'ESPsensor{NODE_ID}_battery',
+            'device' : device,
+            'value_template' : "{{ value_json.bat }}",
+            'force_update' : True,
+            'expire_after' : expire,
+            }
+        client.publish(t, json.dumps(m), qos=0, retain=True)
+
+        t = f'homeassistant/sensor/{NODE_ID}/{name}_orp/config'
+        m = {'device_class' : 'voltage',
+            'unit_of_measurement': 'mV',
+            'name': f'ORP',
+            'state_topic': MQTT_STATE_TOPIC,
+            'unique_id' : f'ESPsensor{NODE_ID}_orp',
+            'device' : device,
+            'value_template' : "{{ value_json.orp }}",
+            'force_update' : True,
+            'expire_after' : expire,
+            }
+        client.publish(t, json.dumps(m), qos=0, retain=True)
+
+        t = f'homeassistant/sensor/{NODE_ID}/{name}_temp/config'
+        m = {'device_class' : 'temperature',
+            'unit_of_measurement': TEMP_CELSIUS, 
+            'name': f'Temperature',
+            'state_topic': MQTT_STATE_TOPIC,
+            'unique_id' : f'ESPsensor{NODE_ID}_temp',
+            'device' : device,
+            'value_template' : "{{ value_json.temp }}",
+            'force_update' : True,
+            'expire_after' : expire,
+            }
+        msg = json.dumps(m).encode('') #for unicode
+        client.publish(t, msg, qos=0, retain=True)
+
+        t = f'homeassistant/sensor/{NODE_ID}/{name}_ph/config'
+        m = {
+            'unit_of_measurement': "pH",
+            'name': f'pH',
+            'icon' : 'mdi:ph',
+            'state_topic': MQTT_STATE_TOPIC,
+            'unique_id' : f'ESPsensor{NODE_ID}_ph',
+            'device' : device,
+            'value_template' : "{{ value_json.ph }}",
+            'force_update' : True,
+            'expire_after' : expire,
+            }
+        client.publish(t, json.dumps(m), qos=0, retain=True)
+
+        t = f'homeassistant/sensor/{NODE_ID}/{name}_fb_ppm/config'
+        m = {
+            'unit_of_measurement': 'ppm',
+            'name': f'Total Bromine',
+            'icon' : 'mdi:chemical-weapon',
+            'state_topic': MQTT_STATE_TOPIC,
+            'unique_id' : f'ESPsensor{NODE_ID}_fb_ppm',
+            'device' : device,
+            'value_template' : "{{ value_json.fb_ppm }}",
+            'force_update' : True,
+            'expire_after' : expire,
+            }
+        client.publish(t, json.dumps(m), qos=1, retain=True) #last one with qos=1 so that we can receive balboa temperature
+
+    def mqtt():
+        wifi(wdt, secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+        client.set_callback(on_message)
+        client.connect()
+        client.subscribe(MQTT_OTA_CMD_TOPIC)
+        client.subscribe(MQTT_BALBOA_TOPIC) # for temperature
+        client.subscribe(MQTT_SLEEP_TOPIC)
+        discovery(client)
+        return client
+
+
+    def measure():
+        set_ldo2_power(True)
+        #set_led(True)
+        wdt.feed()
+
+        #dummy read to lower light sleep current to 1.1ma from 2.4mA
+        #Can anyone explain THIS????
+        ao.read_uv()
+
+        for _ in range(SENSOR_SETTLE_SLEEP_TIMES):
+            my_sleep_ms(s.settle)
+            wdt.feed()
+
+        raw = dict(orp=[], vbat=[], ph=[])
+        for i in range(NUM_SAMPLES):
+            raw['orp'].append(ao.read_uv())
+            raw['vbat'].append(ab.read_uv())
+            raw['ph'].append(ap.read_uv())
+            #if i != NUM_SAMPLES - 1:
+            #    my_sleep_ms(1_000)
+        set_ldo2_power(False)
+        #set_led(False)
+        wdt.feed()
+
+        status = {key : median(vals) for key, vals in raw.items()}
+        status['orp'] = round(status['orp'] / 1000) - 1500 + ORP_CAL_OFFSET #from Atlas Scientific sample code
+        status['vbat'] = round(status['vbat'] / 1000 * 2) #2 resistor voltage divider
+        status['ph_mv'] = round(status['ph'] / 1000)
+
+        # from Atlas Scientific sample code
+        if status['ph_mv'] > PH_MID_CAL:
+            status['ph'] = 7.0 - 3.0 / (PH_LOW_CAL - PH_MID_CAL) * (status['ph_mv'] - PH_MID_CAL)
+        else:
+            status['ph'] = 7.0 - 3.0 / (PH_MID_CAL - PH_HIGH_CAL) * (status['ph_mv'] - PH_MID_CAL)
+
+        status['bat'] = round((status['vbat'] - BAT_LOW) / (BAT_HIGH - BAT_LOW) * 100) 
+        status['bat'] = max(0, min(100, status['bat']))
+
+        return status
+
+    #split measure() from publish() to allow for temperature to come in over mqtt
+
+    def publish(status):
+        #perform PH ATC (Automatic Temperature Compensation)
+        if not CALIBRATION:
+            status['ph'] = round(atc(status['ph'], s.temp), 2)
+        #estimate free chlorine ppm
+        fc = tfmicro.fc(status['orp'], status['ph'])
+        if fc is None:
+            fc = -1.0
+        fc *= FC_TO_TOTAL_BROMINE
+        status['fb_ppm'] = round(fc, 2)
+        status['temp'] = s.temp
+        
+        #qos=1 ensures we complete transaction before going to sleep
+        client.publish(MQTT_STATE_TOPIC, json.dumps(status), qos=1) 
+
+    while True:
+
+        if not s.measured:
+            status = measure()
+            mqtt() #connect MQTT and receive temperature, OTA commands
+            publish(status)
+            #mark success
+            s.measured = True
+            Partition.mark_app_valid_cancel_rollback()
+
+        if s.subscribe_ota:
+            s.subscribe_ota = False
+            client.subscribe(MQTT_OTA_FW_TOPIC)
+
+        if s.prevent_sleep:
+            client.check_msg()
+            continue
+
+        if not DISABLE_DEEPSLEEP:
+            my_go_deepsleep(s.sleep)
+
+        #only get here for debugging
+        print('done')
+        s = State()
+        client.disconnect()
+        #wlan.active(False)
+        for _ in range(5):
+            wdt.feed()
+            time.sleep(1)
---
+++ ports/esp32/modules/main.py
@@ -0,0 +1,69 @@
+from machine import WDT
+class FakeWDT:
+    def __init__(self, timeout):pass
+    def feed(self): pass
+wdt = WDT(timeout=120_000)
+#wdt = FakeWDT(timeout=70000)
+
+import uos
+from umqtt.robust import MQTTClient
+import config
+import secrets
+import io
+from machine import unique_id
+
+NODE_ID = "".join("%x" % b for b in unique_id())
+MQTT_LOG_TOPIC = f'{config.NAME}/{NODE_ID}/logs'
+
+def random_string(length=20):
+    source = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    return ''.join([source[x] for x in [(uos.urandom(1)[0] % len(source)) for _ in range(length)]])
+
+class MyMQTT(MQTTClient):
+    connected = False
+    def connect(self):
+        ret = super().connect()
+        self.connected = True
+        return ret
+    def disconnect(self):
+        ret = super().disconnect()
+        self.connected = False
+        return ret
+
+client = MyMQTT(client_id=random_string(), server=secrets.MQTT_SERVER)
+
+class DUP(io.IOBase):
+    def __init__(self, client):
+        self.s = bytearray()
+        self.client = client
+    def write(self, data):
+        if data == b'\r\n':
+            flush = True
+        else:
+            flush = False
+            self.s += data
+        if flush:
+            if self.client.connected:
+                self.client.publish(MQTT_LOG_TOPIC, self.s, qos=0, retain=True)
+                self.s = bytearray()
+            else:
+                self.s += b'\r\n'
+        return len(data)
+    def readinto(self, data):
+        return 0
+uos.dupterm(DUP(client))
+
+try:
+    import app
+    app.run(client, wdt)
+except Exception as e:
+    if not client.connected:
+        import network
+        wlan = network.WLAN(network.STA_IF)
+        wlan.active(True)
+        if not wlan.isconnected():
+            wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+        while not wlan.isconnected(): pass
+        client.connect()
+    import sys
+    sys.print_exception(e)
